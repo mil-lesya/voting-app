424b2939df7ac127ed7e9a9c2828b136fee51cb1
Ksenia Reut <rieut.ksenia20001@gmail.com>
2022-07-19 17:02:28 +0300

    front basic

---
+++ frontend/assets/constants/colors.js
@@ -0,0 +1,5 @@
+const colors = {
+  white: "#FFFFFF",
+  violet: "#C360FC",
+};
+export default colors;
--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { initContract } from './assets/js/near/utils'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { initContract } from "./assets/js/near/utils";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const container = document.querySelector('#root')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const container = document.querySelector("#root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 window.nearInitPromise = initContract()
   .then(() => {
-    <App />
-    root.render(<App tab="home" />)
+    <App />;
+    root.render(
+      <Router>
+        <App />
+      </Router>
+    );
   })
-  .catch(console.error)
+  .catch(console.error);
