d27a11dc624977a9166385ea4cc89b6987e34fd4
olesia <mileshko.lesya@gmail.com>
2022-08-03 23:50:45 +0300

    tests

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -125,9 +125,7 @@ impl Contract {
             .expect("Such poll does not exists");
         let voter = env::signer_account_id();
 
-        for option in poll.options.iter() {
-            assert_eq!(option.voted_users.contains(&voter), false, "This user has already voted!");
-        }
+        assert_eq!(poll.options.iter().any(|v| v.voted_users.contains(&voter)), false, "This user has already voted!");
 
         let option = poll.options.iter_mut().find(|v| v.id == variant_id).unwrap();
 
@@ -137,7 +135,7 @@ impl Contract {
         self.polls.insert(&poll_id, &poll);
     }
 
-    pub fn is_voted(&self,  account_id: AccountId, poll_id: usize) -> bool {
+    pub fn is_voted(&self, account_id: AccountId, poll_id: usize) -> bool {
         let poll = self.polls
             .get(&poll_id)
             .expect("Such poll does not exists");
@@ -152,15 +150,323 @@ impl Contract {
  */
 #[cfg(test)]
 mod tests {
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::{testing_env};
+
     use super::*;
+
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(accounts(0))
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
+    }
+
 
     #[test]
     fn test_create_poll() {
-        let mut contract = Contract::default();
-        assert_eq!(
-            contract.create_poll("Best programming language!".to_string(), "1656874039533".to_string(), "1656874072974".to_string(), ["Java".to_string(), "Rust".to_string(), "C++".to_string()]),
-            1
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        let created_poll = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options,
         );
-        assert_eq!(contract.polls.get(&0).description.unwrap(), "Best programming language!")
+
+        assert_eq!(created_poll.id, 1);
+        assert_eq!(created_poll.description, description);
+        assert_eq!(created_poll.start_time, Option::from(start_time.to_string()));
+        assert_eq!(created_poll.end_time, Option::from(end_time.to_string()));
+        assert_eq!(created_poll.owner, accounts(1));
     }
+
+    #[test]
+    fn test_get_poll() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options,
+        );
+
+        let poll = contract.get_poll(1).unwrap();
+
+        assert_eq!(poll.id, 1);
+        assert_eq!(poll.description, description);
+        assert_eq!(poll.start_time, Option::from(start_time.to_string()));
+        assert_eq!(poll.end_time, Option::from(end_time.to_string()));
+        assert_eq!(poll.owner, accounts(1));
+    }
+
+    #[test]
+    fn test_delete_poll() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options,
+        );
+
+        contract.delete_poll(1);
+
+        assert_eq!(contract.polls.len(), 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Only owner can delete the poll!")]
+    fn test_delete_poll_not_owned() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options,
+        );
+
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+
+        contract.delete_poll(1);
+        assert_eq!(contract.polls.len(), 1);
+    }
+
+    #[test]
+    fn test_get_polls_for_owner() {
+        let context_1 = get_context(accounts(1));
+        testing_env!(context_1.build());
+
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        let context_2 = get_context(accounts(1));
+        testing_env!(context_2.build());
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        assert_eq!(contract.get_polls_for_owner(accounts(1), None, None).len(), 3);
+        assert_eq!(contract.get_polls_for_owner(accounts(1), Option::from(0), Option::from(1)).len(), 1);
+        assert_eq!(contract.get_polls_for_owner(accounts(1), Option::from(1), Option::from(3)).len(), 2);
+        assert_eq!(contract.get_polls_for_owner(accounts(2), None, None).len(), 0);
+    }
+
+    #[test]
+    fn test_get_all_polls() {
+        let context_1 = get_context(accounts(1));
+        testing_env!(context_1.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        let context_2 = get_context(accounts(1));
+        testing_env!(context_2.build());
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        assert_eq!(contract.get_all_polls(None, None).len(), 3);
+        assert_eq!(contract.get_all_polls(Option::from(0), Option::from(1)).len(), 1);
+        assert_eq!(contract.get_all_polls(Option::from(1), Option::from(3)).len(), 2);
+    }
+
+    #[test]
+    fn test_vote() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        let created_poll = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        contract.vote(created_poll.id, created_poll.options.first().unwrap().id);
+
+        let poll = contract.polls.get(&created_poll.id).unwrap();
+
+        let option = poll.options.iter().find(|v| v.id == poll.options.first().unwrap().id).unwrap();
+
+        assert_eq!(option.voted_users.contains(&accounts(1)), true);
+        assert_eq!(option.votes, 1);
+    }
+
+    #[test]
+    #[should_panic(expected = "This user has already voted!")]
+    fn test_vote_again() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        let created_poll = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        contract.vote(created_poll.id, created_poll.options.first().unwrap().id);
+        contract.vote(created_poll.id, created_poll.options.last().unwrap().id);
+    }
+
+    #[test]
+    fn test_is_voted() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        let created_poll = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        contract.vote(created_poll.id, created_poll.options.first().unwrap().id);
+
+        assert_eq!(contract.is_voted(accounts(1), created_poll.id), true);
+    }
+
+    #[test]
+    fn test_get_voted_polls() {
+        let context_1 = get_context(accounts(1));
+        testing_env!(context_1.build());
+
+        let mut contract = Contract::new_default();
+
+        let description = "Best programming language!";
+        let start_time = "1656874039533";
+        let end_time = "1656874072974";
+        let options = Vec::from(["Java".to_string(), "Rust".to_string(), "C++".to_string()]);
+
+        let created_poll_1 = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        let context_2 = get_context(accounts(0));
+        testing_env!(context_2.build());
+
+        let created_poll_2 = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        let created_poll_3 = contract.create_poll(
+            description.to_string(),
+            Option::from(start_time.to_string()),
+            Option::from(end_time.to_string()),
+            options.clone(),
+        );
+
+        contract.vote(created_poll_1.id, created_poll_1.options.first().unwrap().id);
+        contract.vote(created_poll_2.id, created_poll_2.options.first().unwrap().id);
+        contract.vote(created_poll_3.id, created_poll_2.options.last().unwrap().id);
+
+        assert_eq!(contract.get_voted_polls(accounts(0), None, None).len(), 3);
+        assert_eq!(contract.get_voted_polls(accounts(0), Option::from(0), Option::from(1)).len(), 1);
+        assert_eq!(contract.get_voted_polls(accounts(0), Option::from(1), Option::from(3)).len(), 2);
+        assert_eq!(contract.get_voted_polls(accounts(1), None, None).len(), 0);
+    }
+
+
 }
